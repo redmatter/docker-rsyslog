94ee90d2d70ea7f17e6d33ad68415af3e4d1d278
Dino Korah <dino.korah@redmatter.com>
2016-03-03 18:49:35 +0000

    Adding example app & a working docker-compose yml
    Docker compose file shows how to make use of the syslog container to
    help dockerise legacy syslog-ing applications, without having to make
    them log to console.

---
+++ example-logging-app/html/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.handler' => new Monolog\Handler\SyslogHandler('logging-app')
+));
+
+if (class_exists('Sorien\Provider\PimpleDumpProvider')) {
+    $app->register(new Sorien\Provider\PimpleDumpProvider());
+}
+
+$app->get('/hello/{name}', function($name) use($app) {
+    $app['monolog']->addInfo("Hello $name");
+    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
