6b06071cf7d4ccd29143b6ef1273533a9424a60e
Dino Korah <dino.korah@redmatter.com>
2016-03-04 16:05:37 +0000

    Making PHP code PSR2 compliant

--- example-logging-app/html/index.php
+++ example-logging-app/html/index.php
@@ -4,15 +4,15 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
+$app->register(new Silex\Provider\MonologServiceProvider(), [
     'monolog.handler' => new Monolog\Handler\SyslogHandler('logging-app')
-));
+]);
 
 if (class_exists('Sorien\Provider\PimpleDumpProvider')) {
     $app->register(new Sorien\Provider\PimpleDumpProvider());
 }
 
-$app->get('/hello/{name}', function($name) use($app) {
+$app->get('/hello/{name}', function ($name) use ($app) {
     $app['monolog']->addInfo("Hello $name");
     return 'Hello '.$app->escape($name);
 });
